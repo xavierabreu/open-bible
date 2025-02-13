ebf07b8d9deef4ee6357ec992d28867b8efcd0b5
xavierabreu <xavierabreu00@gmail.com>
2025-02-13 18:21:57 -0500

    Base Structure

---
+++ open-bible/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Open.Bible</h1>
+      <p>Your AI-powered Bible study companion.</p>
+    </div>
+  );
+}
+
+export default App;
